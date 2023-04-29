b92629975423abb1f269d2eef242b87e00b48b9a
JoaoVitor6677 <127797964+JoaoVitor6677@users.noreply.github.com>
2023-04-29 01:37:14 -0300

    Add files via upload

---
+++ segundo_desafio.js
@@ -0,0 +1,27 @@
+/* 
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas:
+1 - Preço do etanol;
+2 - preço da gasolina;
+3 - O tipo de combustível que está no seu carro;
+4 - Gasto médio de combustível do carro por KM;
+5 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+*/
+
+const etanol = 3;
+const gasolina = 5;
+const kmPorLitro = 10;
+const distanciaEmKm = 20;
+const TipoDeCombustivel = (etanol);
+
+if (TipoDeCombustivel === etanol) {
+    console.log (distanciaEmKm  * TipoDeCombustivel / kmPorLitro);
+} 
+else if (TipoDeCombustivel === gasolina) {
+    console.log (distanciaEmKm  * TipoDeCombustivel / kmPorLitro);
+}
+ 
+
