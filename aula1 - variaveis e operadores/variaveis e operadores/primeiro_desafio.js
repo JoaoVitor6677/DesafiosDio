ae02e2b37d9ac28210dbb51e46545c22b7421a32
JoaoVitor6677 <127797964+JoaoVitor6677@users.noreply.github.com>
2023-04-28 23:17:34 -0300

    Add files via upload

---
+++ aula1 - variaveis e operadores/variaveis e operadores/primeiro_desafio.js
@@ -0,0 +1,18 @@
+/*
+
+Faça um programa para calcular o valor de uma viagem.
+
+Voce terá 3 variaveis. Sendo elas:
+1 - Preço do combustível;
+2 - Gasto médio de combustível;
+3 - Distância em KM da viagem;
+
+*/
+
+console.log ('Olá');
+
+const combustivel = 5.79;
+let gasto = 12;
+const km = 1580; 
+
+console.log (km * combustivel / gasto);
