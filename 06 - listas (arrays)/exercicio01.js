a3da080670eca38b41d9cfebfb25da007f336477
JoaoVitor6677 <127797964+JoaoVitor6677@users.noreply.github.com>
2023-05-18 14:10:43 -0300

    Add files via upload

---
+++ 06 - listas (arrays)/exercicio01.js
@@ -0,0 +1,10 @@
+/*
+1- Faça um programa que dado determinado número imprima a tabuada
+*/
+
+const numero = 7
+
+for (let i = 0; i <= 10; i++) {
+    console.log( numero + ' x ' + i + ' = ' + (i * numero));
+}
+
