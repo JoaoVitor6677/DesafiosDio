a23d493e6d92fa910a78d3e464e7b36b4fb76691
JoaoVitor6677 <127797964+JoaoVitor6677@users.noreply.github.com>
2023-05-18 13:41:15 -0300

    Add files via upload

---
+++ 02 - estruturas condicionais/segundo_desafio.js
@@ -0,0 +1,27 @@
+/* 
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas:
+1 - Preço do etanol;
+2 - preço da gasolina;
+3 - O tipo de combustível que está no seu carro;
+4 - Gasto médio de combustível do carro por KM;
+5 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+*/
+
+const etanol = 3;
+const gasolina = 5;
+const kmPorLitro = 10;
+const distanciaEmKm = 20;
+const tipoDeCombustivel = (etanol);
+
+if (tipoDeCombustivel === etanol) {
+    console.log (distanciaEmKm  * tipoDeCombustivel / kmPorLitro);
+} 
+else if (tipoDeCombustivel === gasolina) {
+    console.log (distanciaEmKm  * tipoDeCombustivel / kmPorLitro);
+}
+ 
+
